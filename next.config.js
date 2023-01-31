4628758414bdcaf632b4faec232f6e81b7417c43
rakanishu <rakanishudev@gmail.com>
2023-01-31 17:07:51 +0200

    second commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['cdn.sanity.io'],
-		loader: 'custom'
-  }
 }
 
 module.exports = nextConfig
